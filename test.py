688f2a309de085b31c96790d6debabe0da0bbe00
dycccccccc <165640937+dycccccccc@users.noreply.github.com>
2025-02-04 17:03:50 +0800

    test.py

--- test.py
+++ test.py
@@ -238,7 +238,7 @@ def _cstack(left, right):
         cright = _coord_matrix(right, "right", noutp)
     else:
         cright = np.zeros((noutp, right.shape[1]))
-        cright[-right.shape[0] :, -right.shape[1] :] = right
+        cright[-right.shape[0] :, -right.shape[1] :] = 1
 
     return np.hstack([cleft, cright])
 
